29bf391637c5ae60afcee17cbca694bcab9c76cd
Luke <lmschless@gmail.com>
2020-03-28 23:14:56 -0700

    first commit, adding project to gh

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SphinxRiddles
+{
+    class Program
+    {
+        //public string answerOne = "blue";
+        //public string answerTwo = "green";
+
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            RiddleOne();
+        }
+
+        // void GetAnswer()
+        //{
+        //    string guess = Console.ReadLine();
+        //    string fixedGuess = guess;
+        //}
+
+        public static void RiddleOne()
+        {
+            Console.WriteLine("What is the color of the sky?");
+
+            string guess = Console.ReadLine();
+            while (guess.ToLower() != "blue") {
+                Console.WriteLine("You answered incorrectly, try again!");
+                Console.WriteLine("What is the color of the sky?");
+                guess = Console.ReadLine();
+            }
+            RiddleTwo();
+            
+        }
+
+        public static void RiddleTwo()
+        {
+            Console.WriteLine("What is the color of the grass?");
+            string guess = Console.ReadLine();
+            if (guess.ToLower() == "green")
+            {
+                Console.WriteLine("You won! Play Again? (Y,N)?");
+                if(Console.ReadLine().ToUpper() == "Y")
+                {
+                    RiddleOne();
+                }
+                else
+                {
+                    Console.WriteLine("Thank you, goodbye.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You answered incorrectly, try again!");
+            }
+        }
+    }
+}
